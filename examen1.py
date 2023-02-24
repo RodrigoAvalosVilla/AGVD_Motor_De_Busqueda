bfaf98eb22f058e92d2dd60c440faf72cc11f3d9
Rodrigo Avalos Villa <rodrigoavalos1797@gmail.com>
2023-02-24 09:47:05 -0600

    Comentarios actualizados

--- examen1.py
+++ examen1.py
@@ -1,3 +1,3 @@
 # Realizar el motor de búsqueda con los siguientes criterios:
 # Crear la colección "motor" y la colección debe tener los siguientes documentos:
-# Preguntar que hacer...
+# Preguntar que hacer... Recordar que se hace con MongoDB
